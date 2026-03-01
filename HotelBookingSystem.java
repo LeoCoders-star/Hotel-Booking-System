1d9a833861f88d2ff8fb967a92a2e814646cd8a7
Afiq Leo <afiqleo07@gmail.com>
2026-03-02 00:33:12 +0800

    Project 9 - Hotel Booking System

---
+++ HotelBookingSystem.java
@@ -0,0 +1,38 @@
+package Project9;
+
+import java.util.Scanner;
+
+public class HotelBookingSystem {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int startMenu;
+
+        while (true) {
+            System.out.print("\n=================================");
+            System.out.print("\n       HOTEL BOOKING SYSTEM");
+            System.out.print("\n=================================\n");
+
+            System.out.print("\n1. Add Room");
+            System.out.print("\n2. Display All Rooms");
+            System.out.print("\n5. Exit\n");
+
+            System.out.print("\nEnter choice: ");
+            startMenu = input.nextInt();
+
+            switch (startMenu) {
+                case 1:
+                    
+                    break;
+                
+                case 5:
+                    System.out.print("\nThank You For Using Our System.");
+                    System.out.print("\nSystem Terminated.");
+                    System.exit(0);
+                    
+                default:
+                    break;
+            }
+        }
+    }
+}
